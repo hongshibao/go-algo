b1aaa26798b69e1d4cc94d1b2a1bf75ce28f58e5
hongshibao <shibaohong@outlook.com>
2016-05-22 01:18:29 +0800

    QuickSelect done

--- selection.go
+++ selection.go
@@ -1,18 +1,25 @@
 package algo
 
 import (
-	"errors"
+	"math/rand"
 	"sort"
+
+	"github.com/dropbox/godropbox/errors"
 )
 
-func Partition(array sort.Interface, pivotIndex int) (int, error) {
-	if pivotIndex < 0 || pivotIndex >= array.Len() {
+// [left, right]
+func Partition(array sort.Interface, left int, right int,
+	pivotIndex int) (int, error) {
+	if pivotIndex < left || pivotIndex > right {
 		return -1, errors.New("Pivot index out of range")
 	}
-	lastIndex := array.Len() - 1
+	if left < 0 || right >= array.Len() {
+		return -1, errors.New("Array index out of range")
+	}
+	lastIndex := right
 	array.Swap(pivotIndex, lastIndex)
-	leftIndex := 0
-	for i := 0; i < lastIndex; i++ {
+	leftIndex := left
+	for i := left; i < lastIndex; i++ {
 		if array.Less(i, lastIndex) {
 			array.Swap(leftIndex, i)
 			leftIndex++
@@ -20,4 +27,31 @@ func Partition(array sort.Interface, pivotIndex int) (int, error) {
 	}
 	array.Swap(leftIndex, lastIndex)
 	return leftIndex, nil
+}
+
+// k is started from 1
+func QuickSelect(array sort.Interface, k int) error {
+	if k <= 0 || k > array.Len() {
+		return errors.New("Parameter k is invalid")
+	}
+	k--
+	left := 0
+	right := array.Len() - 1
+	for {
+		if left == right {
+			return nil
+		}
+		pivotIndex := left + rand.Intn(right-left+1)
+		pivotIndex, err := Partition(array, left, right, pivotIndex)
+		if err != nil {
+			return errors.Wrap(err, "Partition returns error")
+		}
+		if k == pivotIndex {
+			return nil
+		} else if k < pivotIndex {
+			right = pivotIndex - 1
+		} else {
+			left = pivotIndex + 1
+		}
+	}
 }
